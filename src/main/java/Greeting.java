78873bebc159e7c9f1d0cc0a7fc230ccabe19604
TheCoteen <knanqurbanov22@gmail.com>
2024-05-06 20:32:22 +0400

    new f

--- src/main/java/Greeting.java
+++ src/main/java/Greeting.java
@@ -2,5 +2,7 @@ public class Greeting {
     public static void main(String[] args) {
         System.out.println("HEllo");
         System.out.println("Hi");
+
+        System.out.println("FUck");
     }
 }
